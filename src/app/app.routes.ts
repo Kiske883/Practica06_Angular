8b19ca15261bd57f650b92a053801400f3fd7073
Lluis Alsina Navarro <lalsina883@gmail.com>
2025-09-19 10:24:25 +0200

    Finalizamos reutilizacion modulo newUser

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full' , redirectTo: 'home'},
+    {path: 'home',component: HomeComponent},
+    {path: 'newUser',component: NewUserComponent},
+    {path: '**', component: ErrorComponent}    
+];
